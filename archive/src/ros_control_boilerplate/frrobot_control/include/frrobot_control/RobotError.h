1f53832c0ed17a4fee433840609e6291a6c9cd05
Lawkaho <luojiahao.cn@outlook.com>
2025-12-12 18:57:11 +0800

    20251212 删除archive旧代码

--- archive/src/ros_control_boilerplate/frrobot_control/include/frrobot_control/RobotError.h
+++
@@ -1,6 +0,0 @@
-#ifndef   ROBOTERROR_H_
-#define   ROBOTERROR_H_
-
-#define     ERR_SUCCESS       0
-
-#endif
--- archive/src/ros_control_boilerplate/frrobot_control/include/frrobot_control/udp_node.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UDP_NODE_H_
-#define __UDP_NODE_H_
-
-#include <ros/ros.h>
-#include <std_msgs/Int64.h>
-#include <std_msgs/String.h>
-#include <mutex>
-#endif
